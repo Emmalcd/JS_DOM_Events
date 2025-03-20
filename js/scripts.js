bf7322911de025b87529529829530e2ec8f73b52
Emmalcd <joselemmanuel9@gmail.com>
2025-03-20 13:53:30 -0600

    Add event listeners for window load and scroll, and handle button click event

--- js/scripts.js
+++ js/scripts.js
@@ -47,3 +47,35 @@ nuevoEnlace.classList.add('navegacion__enlace');
 const navegacion=document.querySelector('.navegacion');
 navegacion.appendChild(nuevoEnlace);
 console.log(navegacion);
+
+// eventos
+
+console.log(1);
+window.addEventListener('load', function () { //Espera a que cargue el JS y los archivos de los que depende el HTML (css, imagenes, etc)
+    console.log(2);
+});
+console.log(3);
+window.onload=function(){
+    console.log(4);
+    //Es exactamente lo mismo que window.addEventListener
+}
+
+window.addEventListener('DOMContentLoaded', function(){ //Solo espera por el HTML (Generalmente mas usado por que no se modifica el css con JS)
+    console.log(10);
+    
+})
+
+window.onscroll = function(){
+    console.log(`Escrollenado . . .`);
+}
+
+// seleccionar un elemento y asociarles un evento 
+
+
+const boton = document.querySelector('.boton--primario');
+
+boton.addEventListener('click', function(event){
+    console.log(event);
+    console.log("Enviando formulario...");
+    event.preventDefault();
+});
