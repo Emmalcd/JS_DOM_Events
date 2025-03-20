f20a4cfe9172466fb8344f654b1cf1cc3a909af7
Emmalcd <joselemmanuel9@gmail.com>
2025-03-20 13:34:37 -0600

    Comment out console logs and add functionality to create and append a new link element

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-console.log(`Funciona correctamente`);
+// console.log(`Funciona correctamente`);
 
 
 // querySelector ->Selecciona uno o cero elementos
@@ -15,8 +15,8 @@ console.log(`Funciona correctamente`);
 // .clase
 // #id
 const enlaces= document.querySelectorAll('.navegacion a');
-console.log(enlaces);
-console.log(enlaces[0]);
+// console.log(enlaces);
+// console.log(enlaces[0]);
 enlaces[0].textContent="Texto X"
 enlaces[0].href="https://x.com/?lang=es";
 enlaces[0].classList.add('nueva__clase1');
@@ -30,3 +30,20 @@ enlaces[0].classList.remove('nueva__clase1')
 const heading = document.getElementById('heading');
 
 heading.textContent="Esto se escribio con un getElementById";
+
+// Crear elementos con JS
+// Generando un nuevo enlace
+const nuevoEnlace=document.createElement('A'); //Se prefiere que las etiquetas HTML se pongan en mayusculas 
+// Agregar el href
+nuevoEnlace.href='nueva-url';
+// Agregar el texto 
+nuevoEnlace.textContent='nuevo texto'
+// Agregar la clase
+nuevoEnlace.classList.add('navegacion__enlace');
+
+
+
+// Agregarlo al HTML
+const navegacion=document.querySelector('.navegacion');
+navegacion.appendChild(nuevoEnlace);
+console.log(navegacion);
